f184104e3f9afe85fae0416b9f8081273dd46092
Adria <adria.blancafort@factorial.co>
2025-10-14 19:40:17 +0200

    mastra connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button"
- 
+import Chat from "@/components/chat"
+
 function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+  return <Chat />
 }
- 
+
 export default App
