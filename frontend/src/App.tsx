36ee5cfa7717079129be6aa59c6336c188c14ed8
Adria <adria.blancafort@factorial.co>
2025-10-14 16:07:26 +0200

    shadcn init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button"
+ 
 function App() {
   return (
-    <p>Hello</p>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
-
+ 
 export default App
